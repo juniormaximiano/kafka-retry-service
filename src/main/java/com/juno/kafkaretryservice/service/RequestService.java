31908aaf3944d08034ead507deb3b84e1a570b09
juniormaximiano <juniormaximianoadeilson@hotmail.com>
2026-07-07 19:39:32 -0300

    feat: add request service operations and improve gitignore

--- src/main/java/com/juno/kafkaretryservice/service/RequestService.java
+++ src/main/java/com/juno/kafkaretryservice/service/RequestService.java
@@ -4,6 +4,9 @@ import com.juno.kafkaretryservice.domain.Request;
 import com.juno.kafkaretryservice.store.RequestStore;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.UUID;
+
 @Service
 public class RequestService {
 
@@ -17,5 +20,15 @@ public class RequestService {
         Request request = new Request(payload);
         return requestStore.save(request);
     }
+
+    public Request findById(UUID id) {
+        return requestStore.findById(id);
+    }
+
+    public Collection<Request> findAll() {
+        return requestStore.findAll();
+    }
+
+
 
 }
